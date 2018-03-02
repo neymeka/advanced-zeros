e68f6b8bfc67b5c1bccec7cb831807609500e528
neymeka <megustafather@gmail.com>
2018-03-02 19:59:57 +0300

    feacher/master: AdvancedZeros ver 1.7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var dividers = [];
+  var counter = 0;
+  var baseCopy = base;
+  var Zeros = Infinity;
+  for (i = 2; i <= baseCopy; i++) {
+      if (baseCopy % i == 0) {
+        var power = 0
+        while (baseCopy % i == 0) {
+          power++;
+          baseCopy /= i;
+        }
+  var result = 0;
+  var iCopy = i;
+  do {
+    counter = Math.floor(number / iCopy);
+    iCopy *= i;
+    result += counter;
+  } while (counter != 0) ;
+
+  var testValue = Math.floor (result / power);
+    if (testValue < Zeros) {
+      Zeros = testValue;
+      }
+    }
+  }
+  return Zeros;
 }
